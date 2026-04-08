d791951bae175f21a63e9ff5789357aaf07e5d0a
Chris Devalme <cdevalme@gmail.com>
2026-04-08 15:40:15 -0400

    Completed functionality

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter how many hours you've worked: ");
+        double hours = scanner.nextDouble();
+
+        System.out.println("Enter your pay rate: ");
+        double payRate = scanner.nextDouble();
+        scanner.nextLine();
+
+        double grossPay;
+
+        if(hours > 40) {
+            grossPay = 40 * payRate;
+            double overtime = hours - 40;
+            grossPay = grossPay + (overtime * (payRate * 1.5));
+            System.out.printf("%s worked %.2f hours for $%.2f dollars an hour. \n Your gross pay is $%.2f", name, hours, payRate, grossPay);
+        }   else {
+            grossPay = hours * payRate;
+            System.out.printf("%s worked %.2f hours for $%.2f dollars an hour. \n Your gross pay is $%.2f", name, hours, payRate, grossPay);
+        }
+    }
 }
