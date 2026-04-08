756563ed4e5ff78d42883b2a5666d88b0eecfc50
Chris Devalme <cdevalme@gmail.com>
2026-04-08 15:44:14 -0400

    Updated syntax with overtimeRate variable.

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -11,17 +11,20 @@ public class PayrollCalculator {
         System.out.println("Enter how many hours you've worked: ");
         double hours = scanner.nextDouble();
 
+
         System.out.println("Enter your pay rate: ");
         double payRate = scanner.nextDouble();
         scanner.nextLine();
+
+        double overtimeRate = payRate * 1.5;
 
         double grossPay;
 
         if(hours > 40) {
             grossPay = 40 * payRate;
             double overtime = hours - 40;
-            grossPay = grossPay + (overtime * (payRate * 1.5));
-            System.out.printf("%s worked %.2f hours for $%.2f dollars an hour. \n Your gross pay is $%.2f", name, hours, payRate, grossPay);
+            grossPay = grossPay + (overtime * overtimeRate);
+            System.out.printf("%s worked %.2f hours for $%.2f dollars an hour. With an overtime rate of $%.2f an hour. \n Your gross pay is $%.2f", name, hours, payRate, overtimeRate, grossPay);
         }   else {
             grossPay = hours * payRate;
             System.out.printf("%s worked %.2f hours for $%.2f dollars an hour. \n Your gross pay is $%.2f", name, hours, payRate, grossPay);
